03eed4f90ebae380728a8b7fa0697f433839b189
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2022-02-09 23:32:03 +1030

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 #include <filesystem>
 
 // Vector containing words to display here:
-std::vector<std::string> vector_test = {"HELLO", "FRIEND", "ABC", "123"};
+std::vector<std::string> vector_test = { "HELLO", "FRIEND", "ABC", "123" };
 
 // Vector with all colour codes here:
-std::vector<std::string> vector_colour_codes = {"\033[31m", "\033[32m", "\033[36m"}; 
+std::vector<std::string> vector_colour_codes = { "\033[31m", "\033[32m", "\033[36m" };
 // Sample to test: "\033[31m", "\033[32m" 
 
 // Function to take text and change colour:
@@ -14,16 +14,15 @@ std::string text_colour_change(std::string para)
     // Use global vector containing colour codes.
 
     // TEST
-    std::string build_text = para + "Sample_Text" + "\033[0m";
+    std::string build_text = para + vector_test[rand() % vector_test.size()] + "\033[0m";
     std::string word_test = "\033[31mSample_Text123\033[0m";
-    return word_test;
+    return build_text;
 }
 
 // Select a random text to display on console.
-void select_text()
+std::string select_text()
 {
-    int text_number = rand() % vector_test.size();
-    return test[text_number];
+    return vector_test[rand() % vector_test.size()];
 }
 
 int main()
@@ -37,13 +36,15 @@ int main()
 
     // STEP 1) Remove instructions.
     system("cls");
-    
+
     // STEP 2) Display text.
-    std::cout << text_colour_change("\033[31m") << "\n";
-    
-    select_text();
 
     // STEP 3) Take text, change colour.
+    std::cout << select_text() << "\n";
+    std::cout << select_text() << "\n";
+    std::cout << select_text() << "\n";
+    std::cout << select_text() << "\n";
+    std::cout << text_colour_change(vector_colour_codes[rand() % vector_test.size()]) << "\n";
 
     // STEP 4) Display text.
 
@@ -54,15 +55,12 @@ int main()
 
 /*
 Project Plan and Design
-
 Brief : Annoy users with rapid and colour changing text on a console.
-
 [Version 1]
 === Minimum Functions ===
 [-] Display text on console application and change to different text within half a second.
 [-] Different text are displayed in different colours
 [-] Clear console after every text change.
 [-]
-
 === TODO ===
 */

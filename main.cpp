24af514a4f47a2ee2d827150bf24507aa76b8ce2
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2022-02-12 09:45:12 +1030

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,29 +3,22 @@
 #include <windows.h>
 
 // Vector containing words to display here:
-std::vector<std::string> vector_test = { "HELLO", "FRIEND", "ABC", "123", "Monarch" };
+std::vector<std::string> vector_word_list = { "HELLO", "FRIEND", "ABC", "123", "Monarch" };
 
 // Vector with all colour codes here:
 std::vector<std::string> vector_colour_codes = { "\033[31m", "\033[32m", "\033[33m", "\033[34m", "\033[35m", "\033[36m", "\033[37m" };
 
 int generate_rand_number()
 {
-    return rand() % vector_test.size();
+    return rand() % vector_word_list.size();
 }
 
 // Function to take text and change colour:
-std::string text_colour_change(std::string para)
+std::string text_colour_change(std::string colour_code)
 {
     // Use global vector containing colour codes.
-    std::string build_text = para + vector_test[generate_rand_number()] + "\033[0m";
-    std::string word_test = "\033[31mSample_Text123\033[0m";
+    std::string build_text = colour_code + vector_word_list[generate_rand_number()] + "\033[0m";
     return build_text;
-}
-
-// Select a random text to display on console.
-std::string select_text()
-{
-    return vector_test[generate_rand_number()];
 }
 
 int main()
@@ -37,35 +30,22 @@ int main()
     std::cout << "- Current location of executable: " << std::filesystem::current_path() << "\n";
     std::cout << "=======================================" << "\n\n";
 
-    // STEP 1) Remove instructions.
-    system("cls");
-
-    // STEP 2) Display text.
-
-    // STEP 3) Take text, change colour.
     while (true)
     {
         system("cls");
-        //std::cout << select_text() << "\n";
         std::cout << text_colour_change(vector_colour_codes[rand() % vector_colour_codes.size()]) << "\n";
-        Sleep(50);
+        Sleep(150);
     }
-
-    // STEP 4) Display text.
-
-    // STEP 5) Change text.
-
 }
-
 
 /*
 Project Plan and Design
 Brief : Annoy users with rapid and colour changing text on a console.
 [Version 1]
 === Minimum Functions ===
-[-] Display text on console application and change to different text within half a second.
-[-] Different text are displayed in different colours
-[-] Clear console after every text change.
+[+] Display text on console application and change to different text within half a second.
+[+] Different text are displayed in different colours
+[+] Clear console after every text change.
 [-]
 === TODO ===
 */

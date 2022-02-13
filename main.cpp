915d3848c52014bd41218bc40d3eee8bab680b9b
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2022-02-13 17:51:03 +1030

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <windows.h>
 
 // Vector containing words to display here:
-std::vector<std::string> vector_word_list = { "HELLO", "FRIEND", "ABC", "123", "Monarch" };
+std::vector<std::string> vector_word_list = { "HELLO", "FRIEND", "ABC", "123", "Monarch", "QWERTY" };
 
 // Vector with all colour codes here:
 std::vector<std::string> vector_colour_codes = { "\033[31m", "\033[32m", "\033[33m", "\033[34m", "\033[35m", "\033[36m", "\033[37m" };
@@ -30,6 +30,17 @@ int main()
     std::cout << "- Current location of executable: " << std::filesystem::current_path() << "\n";
     std::cout << "=======================================" << "\n\n";
 
+    // https://stackoverflow.com/questions/35382432/how-to-change-the-console-font-size
+    CONSOLE_FONT_INFOEX cfi;
+    cfi.cbSize = sizeof(cfi);
+    cfi.nFont = 0;
+    cfi.dwFontSize.X = 0;
+    cfi.dwFontSize.Y = 30;
+    cfi.FontFamily = FF_DONTCARE;
+    cfi.FontWeight = FW_NORMAL;
+    //std::wcscpy(cfi.FaceName, L"Consolas"); // Choose your font
+    SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
+
     while (true)
     {
         system("cls");
@@ -46,6 +57,4 @@ Brief : Annoy users with rapid and colour changing text on a console.
 [+] Display text on console application and change to different text within half a second.
 [+] Different text are displayed in different colours
 [+] Clear console after every text change.
-[-]
-=== TODO ===
 */
